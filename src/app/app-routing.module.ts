ab389515ad6ace07d4d73fad7129367a0e075081
PrashantKat18 <prashantkatiyar10796@gmail.com>
2021-11-28 23:54:50 +0530

    added experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,6 @@ const routes: Routes = [
   },
   {
     path:'projects', 
-    canActivateChild:[ActivatechildguardGuard],
     loadChildren: () => import('./pages/project/project.module').then(m => m.ProjectModule)
   },
   {
