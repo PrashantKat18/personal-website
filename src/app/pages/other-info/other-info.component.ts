8c86d48e250d974baa41a3950d92ba8cb0afb3c7
Prashant Katiyar <prashantkat10796@gmail.com>
2023-10-22 23:56:14 +0530

    Added build file

--- src/app/pages/other-info/other-info.component.ts
+++ src/app/pages/other-info/other-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { MessageComponent } from '../../../../src/app/components/message/message.component';
@@ -10,7 +10,9 @@ import * as AOS from 'aos';
   styleUrls: ['./other-info.component.scss']
 })
 export class OtherInfoComponent implements OnInit {
-
+  showScroll: boolean;
+  showScrollHeight = 300;
+  hideScrollHeight = 10;
   constructor(public addProvider: AddProvider,public dialog: MatDialog,private router: Router,) { }
 
   ngOnInit() {
@@ -31,14 +33,29 @@ export class OtherInfoComponent implements OnInit {
   }
 
 
-  scrollToTop() {
-    (function smoothscroll() {
-      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-      if (currentScroll > 0) {
-        window.requestAnimationFrame(smoothscroll);
-        window.scrollTo(0, currentScroll - (currentScroll / 5));
-      }
-    })();
+  @HostListener('window:scroll', [])
+  onWindowScroll() 
+  {
+    if (( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight) 
+    {
+        this.showScroll = true;
+    } 
+    else if ( this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight) 
+    { 
+      this.showScroll = false; 
+    }
+  }
+
+  scrollToTop() 
+    { 
+      (function smoothscroll() 
+      { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; 
+        if (currentScroll > 0) 
+        {
+          window.requestAnimationFrame(smoothscroll);
+          window.scrollTo(0, currentScroll - (currentScroll / 5));
+        }
+      })();
   }
 
   
