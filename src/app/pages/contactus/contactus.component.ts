2cffbca065e865743f7ece9f5f627a71d35006fb
Prashant Katiyar <prashantkatiyar10796@gmail.com>
2025-08-17 12:11:48 +0530

    Added email form and update some msg

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as AOS from 'aos';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
@@ -9,7 +11,7 @@ import * as AOS from 'aos';
 export class ContactusComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
@@ -25,20 +27,68 @@ export class ContactusComponent implements OnInit {
   // convenience getter for easy access to form fields
   get f() { return this.registerForm.controls; }
 
+
   onSubmit() {
     this.submitted = true;
+
     if (this.registerForm.invalid) {
       return;
     }
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+
+    // 🔄 Loading spinner
+    Swal.fire({
+      title: '📩 Sending Your Message...',
+      text: 'Hang tight! We are processing your request.',
+      background: '#f4f9ff',
+      color: '#2c3e50',
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
+    const formData = this.registerForm.value;
+
+    this.http.post('https://formspree.io/f/myzpaekl', formData).subscribe({
+      next: () => {
+        Swal.fire({
+          icon: 'success',
+          title: '✅ Thank You!',
+          text: 'Your message has been sent successfully. We will get back to you shortly.',
+          background: '#e8f9f1',
+          color: '#14532d',
+          confirmButtonText: 'Close',
+          confirmButtonColor: '#16a34a',
+          timer: 5000,
+          timerProgressBar: true,
+          allowOutsideClick: false,
+          allowEscapeKey: false
+        });
+        this.registerForm.reset();
+        this.submitted = false;
+      },
+      error: () => {
+        Swal.fire({
+          icon: 'error',
+          title: '⚠️ Oops!',
+          text: 'Something went wrong. Please try again later.',
+          background: '#fff5f5',
+          color: '#7f1d1d',
+          confirmButtonText: 'Retry',
+          confirmButtonColor: '#dc2626',
+          timer: 5000,
+          timerProgressBar: true
+        });
+      }
+    });
   }
+
 
   onReset() {
     this.submitted = false;
     this.registerForm.reset();
   }
-  
+
 
   scrollToTop() {
     (function smoothscroll() {
@@ -49,7 +99,7 @@ export class ContactusComponent implements OnInit {
       }
     })();
   }
-  
+
 
 }
 
